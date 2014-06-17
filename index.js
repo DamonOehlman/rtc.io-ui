0c60cb669e12b761228c151f0b33fd3ed1a249b4
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-17 12:59:32 +1000

    Add removePeerVideo implementation

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+  # rtc-ui
+
+  This is a collection of user interface helper functions that can be used
+  in conjunction with other rtc.io modules.
+
+  ## Reference
+**/
+
+exports.removePeerVideo = require('./removePeerVideo');
